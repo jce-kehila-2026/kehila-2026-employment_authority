4f06891f83c86916886075aa92c3655fa0ae7c05
Nadav_vega <nadavbega@gmail.com>
2026-04-23 21:13:38 +0300

    added my part of the installation assignment

---
+++ employment-authority/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Employment Authority Project</h1>
+      <p>My clean Vite + React app is running successfully.</p>
+    </div>
+  );
+}
+
+export default App;
